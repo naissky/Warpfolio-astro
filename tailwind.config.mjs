312d95ac7f2df5d9d7033650a3e4e65ca2e8ce40
Fabian Segovia <88550985+naissky@users.noreply.github.com>
2025-01-15 12:52:14 -0500

    add: Hero Section and Header Responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				shark: {
+					50: '#f6f6f6',
+					100: '#e7e7e7',
+					200: '#d1d1d1',
+					300: '#b0b0b0',
+					400: '#888888',
+					500: '#6d6d6d',
+					600: '#5d5d5d',
+					700: '#4f4f4f',
+					800: '#454545',
+					900: '#3d3d3d',
+					950: '#1e1e1e',
+				},
+			},
+		},
 	},
 	plugins: [],
 }

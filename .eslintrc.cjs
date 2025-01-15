69ce1dd8c64e327164020f3c6a329b1c7c30e9bd
Fabian Segovia <88550985+naissky@users.noreply.github.com>
2025-01-15 08:56:43 -0500

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'quotes': ['error', 'single'],
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      semi: true,
+      tabWidth: 2,
+    }],
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  ],
+}
